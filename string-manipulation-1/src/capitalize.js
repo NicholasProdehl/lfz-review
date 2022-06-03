c06f95cfb8b0ddfd86197488c3ecb6491734c395
Nicholas Prodehl <Nicholasprodehl@gmail.com>
2022-06-03 19:55:06 +0000

    Updated Master

--- string-manipulation-1/src/capitalize.js
+++
@@ -1,8 +0,0 @@
-/* exported capitalize */
-function capitalize(word){
-  let firstLetter
-  let lowerStringWord = word.slice(1).toLowerCase();
-  firstLetter = word[0].toUpperCase();
-  let finalWord = firstLetter + lowerStringWord;
-  return finalWord;
-}
--- string-manipulation-1/src/get-words.js
+++
@@ -1,9 +0,0 @@
-/* exported getWords */
-function getWords(string){
-  let finalArray = []
-  if(string){
-    finalArray = string.split(" ");
-    return finalArray;
-  }
-  return finalArray;
-}
