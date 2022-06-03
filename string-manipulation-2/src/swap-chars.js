7c536586a8c96e759d3e30b67bf29cffa2778838
Nicholas Prodehl <Nicholasprodehl@gmail.com>
2022-06-03 19:53:39 +0000

    Finished String Manipulation 2

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,16 @@
 /* exported swapChars */
+function swapChars(firstIndex, secondIndex, string){
+  let newString = '';
+  console.log("second Index: ", string[secondIndex]);
+  for(let i = 0; i < string.length; i++){
+    if(i === secondIndex){
+      newString += string[firstIndex];
+    }
+    else if(i === firstIndex){
+        newString += string[secondIndex];
+    } else{
+      newString += string[i];
+    }
+  }
+  return newString;
+}
